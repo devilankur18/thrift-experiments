6b5b4eae79ca42eda576fa5214ac3c69ed2824e6
Ankur <signup.ankur@gmail.com>
2012-04-29 23:41:46 +0530

    Adding files

---
+++ log.thrift
@@ -0,0 +1,16 @@
+enum ResultCode
+{
+  OK,
+  TRY_LATER
+}
+
+struct LogEntry
+{
+  1:  string category,
+  2:  string message
+}
+
+service scribe extends fb303.FacebookService
+{
+  ResultCode Log(1: list<LogEntry> messages);
+}
